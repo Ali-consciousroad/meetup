4b2ee7fe7acf9acc6fa346942ec2b4472c68f28c
Ali-consciousroad <ali_dindar@live.be>
2021-06-02 18:13:32 +0200

    29.471 Sending a POST request with the fetch() function

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,7 +2,18 @@ import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage(){
     function addMeetupHandler(meetupData) {
-
+        // Use the fetch() function to do a POST HTTP request to our Firebase API
+        // Modify the URL to create a meetups table when the request is done (.json is required by firebase)
+        fetch(
+            'https://react-meetup-d02df-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
     }
 
     return (
